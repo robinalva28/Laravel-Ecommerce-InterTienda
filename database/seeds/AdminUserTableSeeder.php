6ea196e2d47359e569eb2c06285ec6bd8f061b93
robinalva28 <robinnet28@hotmail.com>
2020-04-10 21:13:22 -0300

    Agregado seeder para crear usuario administrador

---
+++ database/seeds/AdminUserTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminUserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+
+        DB::table('usuarios')->insert([
+            'nombre' => 'Administrador',
+            'apellido' => 'Supremo',
+            'email' => 'admin@admin.com',
+            'celular'=>'123456789',
+            'fechaNacimiento'=>'1999-02-22',
+            //'usrIdEmpresa'=> null,
+            'cuilEmpresa'=>'12345678910',
+            'password' => Hash::make('12345admin'),
+            'avatar'=> 'Forrest-Gump.jpg',
+            'validado' => 1,
+            'isAdmin' => 1
+        ]);
+
+
+
+    }
+}
