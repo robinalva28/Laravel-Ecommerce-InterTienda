591dc9c2d97a6b4a47ff89c15ab46de6f134a2d8
robinalva28 <robinnet28@hotmail.com>
2020-06-21 22:14:43 -0300

    modif seeder de BD

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,32 +14,49 @@ class DatabaseSeeder extends Seeder
         /*CREACIÓN DE VARIAS MARCAS*/
 
         DB::table('marcas')->insert([
-            'marNombre'=>'Xiaomi'],[
-            'marNombre'=>'Samsung'],[
-             'marNombre'=>'Oster'],[
-             'marNombre'=>'Patrick'],[
-             'marNombre'=>'Quicksilver'],[
-             'marNombre'=>'Zara'],[
-             'marNombre'=>'JBL'],[
-             'marNombre'=>'Adidas'],[
+            'marNombre'=>'Xiaomi']
+        );
+
+        DB::table('marcas')->insert([
+            'marNombre'=>'Samsung']
+            );
+        DB::table('marcas')->insert([
+             'marNombre'=>'Oster']
+            );
+        DB::table('marcas')->insert([
+             'marNombre'=>'Patrick']
+            );
+        DB::table('marcas')->insert([
+             'marNombre'=>'Quicksilver']
+            [);
+        DB::table('marcas')->insert([
+             'marNombre'=>'Zara']
+            );
+        DB::table('marcas')->insert([
+             'marNombre'=>'JBL']
+            );
+        DB::table('marcas')->insert([
+             'marNombre'=>'Adidas']
+            );
+        DB::table('marcas')->insert([
              'marNombre'=>'Otra marca'
         ]);
 
 /*CREACION DE VARIAS CATEGORIAS*/
-
+        
         DB::table('categorias')->insert([
               'catNombre'=>'Teconología',
               'catDescripcion'=>'Cualquier equipo tecnológico que quieras comprar',
-              'catImagen'=>'cateroria_tecnologia.png'],
-            [
+              'catImagen'=>'cateroria_tecnologia.png' ]);
+        DB::table('categorias')->insert([ [
              'catNombre'=>'Vestimenta',
               'catDescripcion'=>'Cualquier ropa o calzado que quieras comprar',
-              'catImagen'=>'cateroria_vestimenta.png'],
-            [
+              'catImagen'=>'cateroria_vestimenta.png' ]);
+        DB::table('categorias')->insert([
              'catNombre'=>'Hogar',
               'catDescripcion'=>'Todo para el hogar',
-              'catImagen'=>'otros_01.png'],
-            [
+              'catImagen'=>'otros_01.png' ]);
+        DB::table('categorias')->insert([
              'catNombre'=>'Otros + Servicios',
               'catDescripcion'=>'Otros productos mas servicios ofrecidos',
               'catImagen'=>'cateroria_otros.png'
